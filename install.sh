86eb2063d166f593c71313db7fc95b5f463004f1
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-08-01 14:50:10 +0200

    Update installer.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+spack bootstrap now
+
 envs=(benchmark-euler-kernels ddc gmgpolar gyselalibxx heraclespp kokkos kokkos-fft)
 
 for env in "${envs[@]}"
 do
   spack env remove --yes-to-all ${env} || true
   spack env create ${env} dev-${env}.yaml
+  spack --env ${env} repo update
+  spack --env ${env} buildcache list
   spack --env ${env} install --include-build-deps --use-buildcache only
 done
