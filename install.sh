9123c13a6c5825087b95353860dbb1a069c311b4
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-09-24 19:42:25 +0200

    Add dev Kokkos Comm

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 spack bootstrap now
 
-envs=(benchmark-euler-kernels ddc gmgpolar gyselalibxx gyselaxpp heraclespp kokkos kokkos-fft mini-app-io mini-app-poisson)
+envs=(benchmark-euler-kernels ddc gmgpolar gyselalibxx gyselaxpp heraclespp kokkos kokkos-comm kokkos-fft mini-app-io mini-app-poisson)
 
 for env in "${envs[@]}"
 do
